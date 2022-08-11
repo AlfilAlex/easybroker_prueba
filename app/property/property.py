eeaa6783736741d747e7e17b152baa77f8b7cb18
AlfilAlex <cabimax@outlook.com>
2022-08-11 17:32:40 -0500

    Integración de form contact

--- app/property/property.py
+++ app/property/property.py
@@ -17,13 +17,11 @@ BASE_URL = app.config['BASE_URL']
 EB_TOKEN = app.config['EB_TOKEN']
 
 
-# @cross_origin(supports_credentials=True, origins=['*'], allow_headers=['Content-Type', 'Authorization'])
 @property_profile.route(f'/properties/<property_id>', methods=['GET'])
 def property_page(property_id):
     res = requests.get(f'{BASE_URL}/properties/{property_id}', headers={
         'X-Authorization': EB_TOKEN})
     property_info = res.json()
-    # print(property_info)
 
     return render_template('property.html', property_info=property_info)
 
