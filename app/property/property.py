985b1d4bb2dbdf141c73bd61a5a77a0d6f63b9f5
AlfilAlex <cabimax@outlook.com>
2022-08-11 17:28:16 -0500

    Form y endpoint creado para generar contacto

--- app/property/property.py
+++ app/property/property.py
@@ -1,6 +1,6 @@
 import requests
 
-from flask import render_template, make_response, jsonify
+from flask import render_template, request, make_response
 from flask import current_app as app
 from flask import Blueprint
 
@@ -23,16 +23,23 @@ def property_page(property_id):
     res = requests.get(f'{BASE_URL}/properties/{property_id}', headers={
         'X-Authorization': EB_TOKEN})
     property_info = res.json()
-    print(property_info)
+    # print(property_info)
 
     return render_template('property.html', property_info=property_info)
 
 
 @property_profile.route(f'/properties/<property_id>', methods=['POST'])
 def property_contact(property_id):
-    # res = requests.get(f'{BASE_URL}/properties/{property_id}', headers={
-    #     'X-Authorization': EB_TOKEN})
-    # property_info = res.json()
-    # print(property_info)
+    form_data = request.form.to_dict()
+    form_data['property_id'] = property_id
+    form_data['source'] = 'cabimax.com'
 
-    return render_template('succesfull_contact.html')
+    res = requests.post(f'{BASE_URL}/contact_requests', json=form_data, headers={
+        'X-Authorization': EB_TOKEN,
+        "accept": "application/json",
+        'content-type': 'application/json'})
+
+    if res.ok:
+        return render_template('succesfull_contact.html')
+    else:
+        make_response({'error': res.json()}, 400)
